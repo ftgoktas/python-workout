6cb2bba58f68babc6762aadb8934a6a456a47d05
ftgoktas <goktas@cs.fsu.edu>
2021-09-26 12:29:14 -0400

    added code that encrypts data stored in a database

--- newAgent.py
+++ newAgent.py
@@ -11,8 +11,9 @@ the SecurityLevel is a numeric value between 1 and 10 inclusive.
 All work below was performed by Furkan T Goktas
 
 """
+import os
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, flash, jsonify
 import sqlite3 as sql
 
 app = Flask(__name__)
@@ -21,13 +22,17 @@ app = Flask(__name__)
 # default routing
 @app.route('/')
 def home():
-    return render_template('home.html')
-
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return render_template('home.html', name=session['name'])
 
 @app.route('/enternew')
 def new_student():
-    return render_template('agent.html')
-
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return render_template('agent.html')
 
 @app.route('/addrec', methods=['POST', 'GET'])
 def addrec():
@@ -80,18 +85,68 @@ def addrec():
             return render_template("result.html", msg=msg)
             con.close()
 
-
 @app.route('/list')
 def list():
-    con = sql.connect("AgentDB.db")
-    con.row_factory = sql.Row
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        con = sql.connect("AgentDB.db")
+        con.row_factory = sql.Row
 
-    cur = con.cursor()
-    cur.execute("select * from AGENTS")
+        cur = con.cursor()
+        cur.execute("select * from AGENTS")
 
-    rows = cur.fetchall();
-    return render_template("list.html", rows=rows)
+        rows = cur.fetchall();
+        return render_template("list.html", rows=rows)
 
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    try:
+        nm = request.form['username']
+        pwd = request.form['password']
+
+        with sql.connect("CrapsDB.db") as con:
+            con.row_factory = sql.Row
+            cur = con.cursor()
+
+            sql_select_query = """select * from player where Name = ? and Password = ?"""
+            cur.execute(sql_select_query, (nm,pwd))
+
+            row = cur.fetchone();
+            if (row != None):
+                session['name'] = nm
+                session['logged_in'] = True
+            else:
+                session['logged_in'] = False
+                flash('invalid username and/or password!')
+    except:
+        con.rollback()
+        flash("error in insert operation")
+    finally:
+        con.close()
+    return home()
+    #return render_template('home.html', name=session['name'])
+
+@app.route("/logout")
+def logout():
+    session['name'] = ""
+    session['logged_in'] = False
+    return home()
 
 if __name__ == '__main__':
+    app.secret_key = os.urandom(12)
     app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
