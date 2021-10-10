c6594f50f7a878f378843cc83873d7e01c0b58e2
Furkan Taha Goktas <goktas@cs.fsu.edu>
2021-10-10 00:06:14 -0400

    Update newAgent.py
    Role-based access control added

--- newAgent.py
+++ newAgent.py
@@ -1,16 +1,3 @@
-"""
-
-Name:Furkan T Goktas
-Date: 9/17/2021
-Assignment:#3 Flask Website
-Due Date: 9/19/2021
-About this project: Developing a frontend application that interacts with a database
-for a small scale real-world applications using third-party Python libraries discussed in the course.
-Assumptions:The AgentName, the AgentAlias and the LoginPassword are not empty and does not only contain spaces,
-the SecurityLevel is a numeric value between 1 and 10 inclusive.
-All work below was performed by Furkan T Goktas
-
-"""
 import os
 
 from flask import Flask, render_template, request, session, flash, jsonify
@@ -27,70 +14,81 @@ def home():
     else:
         return render_template('home.html', name=session['name'])
 
+
 @app.route('/enternew')
-def new_student():
+def new_agent():
     if not session.get('logged_in'):
         return render_template('login.html')
-    else:
+    elif session.get('admin') == True:
         return render_template('agent.html')
+    else:
+        msg = "Page not found"
+        return render_template("result.html", msg=msg)
+
 
 @app.route('/addrec', methods=['POST', 'GET'])
 def addrec():
-    if request.method == 'POST':
-        # form fields
-        try:
-            nm = request.form['Name']
-            als = request.form['Alias']
-            slvl = request.form['SecLvl']
-            pwd = request.form['Password']
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    elif session.get('admin') == True:
+        #if request.method == 'POST':
+            try:
+                msg = ""
+                isValid = True
+                nm = request.form['Name']
+                als = request.form['Alias']
+                slvl = request.form['SecurityLevel']
+                pwd = request.form['Password']
 
-            msg = ""
-            isValid = True
+                # Check if name is not empty and does not only contain spaces
+                if len(nm) < 1 or nm.isspace():
+                    msg += "You cannot enter in an empty name\n"
+                    isValid = False
 
-            # Check if name is not empty and does not only contain spaces
-            if len(nm) < 1 or nm.isspace():
-                msg += "You cannot enter in an empty name\n"
-                isValid = False
+                # Check if alias is not empty and does not only contain spaces
+                if len(als) < 1 or als.isspace():
+                    msg += "You cannot enter in an alias\n"
+                    isValid = False
 
-            # Check if alias is not empty and does not only contain spaces
-            if len(als) < 1 or als.isspace():
-                msg += "You cannot enter in an alias\n"
-                isValid = False
+                # Check if password is not empty and does not only contain spaces
+                if len(pwd) < 1 or pwd.isspace():
+                    msg += "You cannot enter in an empty password\n"
+                    isValid = False
 
-            # Check if password is not empty and does not only contain spaces
-            if len(pwd) < 1 or pwd.isspace():
-                msg += "You cannot enter in an empty password\n"
-                isValid = False
+                # Check if the SecurityLevel is a numeric value between 1 and 10 inclusive
+                if len(slvl) < 1 or slvl.isspace() or not slvl.isnumeric() or int(slvl) >= 10 or int(slvl) <= 0:
+                    msg = msg + "The security level must be a numeric value between 1 and 10."
+                    isValid = False
 
-            # Check if the SecurityLevel is a numeric value between 1 and 10 inclusive
-            if len(slvl) < 1 or slvl.isspace() or not slvl.isnumeric() or int(slvl) >= 10 or int(slvl) <= 0:
-                msg += "The security level must be a numeric value between 1 and 10."
-                isValid = False
+                if isValid:
+                    try:
+                        with sql.connect("CrapsDB.db") as con:
+                            cur = con.cursor()
 
-            if isValid:
-                try:
-                    with sql.connect("AgentDB.db") as con:
-                        cur = con.cursor()
+                        cur.execute("INSERT INTO AGENTS (AgentName,Alias,SecurityLevel,Password) VALUES (?,?,?,?)",
+                                        (nm, als, slvl, pwd))
+                        con.commit()
+                        msg = "Record successfully added"
+                    except:
+                        con.rollback()
+                        msg = "Error in insert operation"
+            except:
+                con.rollback()
+                msg = "Error in insert operation"
 
-                    cur.execute("INSERT INTO AGENTS (Name,Alias,SecLvl,Password) VALUES (?,?,?,?)",
-                                (nm, als, slvl, pwd))
-                    con.commit()
-                    msg += "Record successfully added"
+            finally:
+                return render_template("result.html", msg = msg)
+                con.close()
+    else:
+        return render_template('login.html')
 
-                except:
-                    con.rollback()
-                    msg = "error in insert operation"
-
-        finally:
-            return render_template("result.html", msg=msg)
-            con.close()
 
 @app.route('/list')
 def list():
     if not session.get('logged_in'):
         return render_template('login.html')
-    else:
-        con = sql.connect("AgentDB.db")
+    elif session.get('admin') == True:
+        con = sql.connect("CrapsDB.db")
         con.row_factory = sql.Row
 
         cur = con.cursor()
@@ -99,23 +97,49 @@ def list():
         rows = cur.fetchall();
         return render_template("list.html", rows=rows)
 
+    elif session.get('admin2') == True:
+        con = sql.connect("CrapsDB.db")
+        con.row_factory = sql.Row
+
+        cur = con.cursor()
+        cur.execute("select AgentName, Alias, SecurityLevel from AGENTS")
+
+        rows = cur.fetchall();
+        return render_template("list.html", rows=rows)
+    else:
+        msg = "Page not found"
+        return render_template("result.html", msg=msg)
+
+
 @app.route('/login', methods=['POST'])
 def do_admin_login():
     try:
-        nm = request.form['username']
-        pwd = request.form['password']
+        nm = request.form['AgentName']
+        pwd = request.form['Password']
 
         with sql.connect("CrapsDB.db") as con:
             con.row_factory = sql.Row
             cur = con.cursor()
 
-            sql_select_query = """select * from player where Name = ? and Password = ?"""
-            cur.execute(sql_select_query, (nm,pwd))
+            sql_select_query = """select * from AGENTS where AgentName = ? and Password = ?"""
+            cur.execute(sql_select_query, (nm, pwd))
 
             row = cur.fetchone();
             if (row != None):
-                session['name'] = nm
                 session['logged_in'] = True
+                session['name'] = nm
+                if int(row['SecurityLevel']) == 1:
+                    session['admin'] = True
+                else:
+                    session['admin'] = False
+                if int(row['SecurityLevel']) == 2:
+                    session['admin2'] = True
+                else:
+                    session['admin2'] = False
+                if int(row['SecurityLevel']) == 3:
+                    session['admin3'] = True
+                else:
+                    session['admin3'] = False
             else:
                 session['logged_in'] = False
                 flash('invalid username and/or password!')
@@ -125,28 +149,16 @@ def do_admin_login():
     finally:
         con.close()
     return home()
-    #return render_template('home.html', name=session['name'])
+
 
 @app.route("/logout")
 def logout():
     session['name'] = ""
     session['logged_in'] = False
+    session['admin'] = False
     return home()
+
 
 if __name__ == '__main__':
     app.secret_key = os.urandom(12)
     app.run(debug=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
